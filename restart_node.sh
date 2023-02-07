5defc7302cd8d130b771734b208638921f276e55
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 19:56:14 +0700

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 echo `date +%d/%m/%Y\ %H:%M:%S`;
 echo -e "\nCheck service started or not "
-query21=$(/usr/local/bin/pm2 list | grep "operator-gui")
-# neu nhu empty --> chua co service
-if [[ -z $query21 ]]; then 
-    echo -e "\nOperator GUI not start --> Starting Operator GUI ... ";	   
-    /usr/local/bin/node /usr/local/bin/operator-cli gui start;
-    sleep 1
-    echo -e "\nGUI started!"
-fi
-
-# Check service is stop or not
 query22=$(/usr/local/bin/pm2 list | grep operator-gui | grep stopped) 
 if [[ -n $query22 ]]; then 
     echo -e "\nGUI stopped --> Start GUI again ...";	   
     /usr/local/bin/node /usr/local/bin/operator-cli gui start
     sleep 1
-    echo -e "\nValidator started"
+    echo -e "\nGUI started"
 else
     echo -e "\nGUI still running"
-    echo -e $query22
-fi
-
-query31=$(/usr/local/bin/pm2 list | grep validator)
-# neu nhu empty --> chua co service
-if [[ -z $query31 ]]; then 
-    echo -e "\nValidator not start --> Starting Validator ... ";	   
-    /usr/local/bin/node /usr/local/bin/operator-cli start;
-    sleep 2
-    echo -e "\nValidator started!"
 fi
 
 query32=$(/usr/local/bin/pm2 list | grep "validator" | grep stopped)
@@ -39,5 +19,4 @@ if [[ -n $query32 ]]; then
     echo -e "\nValidator started!"
 else
     echo -e "\nValidator still running"
-    echo -e $query32
 fi
