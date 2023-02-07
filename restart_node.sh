7234b147918722feac11aa2dab06b331a0d655b1
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 18:02:53 +0700

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -3,11 +3,13 @@ echo `date +%d/%m/%Y\ %H:%M:%S`;
 query=$(/usr/local/bin/pm2 list | grep validator | grep stopped) 
 if [[ -n $query ]]; then 
     echo -n "#Validator stopped --> Start validator again ...";	   
-    operator-cli start;
+    operator-cli start
+    echo -n "Validator started"
 fi
 
 query2=$(/usr/local/bin/pm2 list | grep "operator-gui" | grep stopped) 
 if [[ -n $query2 ]]; then 
     echo -n "#Operator GUI stopped --> Start Operator GUI again ...";	   
     operator-cli gui start;
+    echo -n "GUI started!"
 fi
