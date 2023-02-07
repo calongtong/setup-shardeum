c7d01ea14d3f9dd140bf73ece7ed4d4b7b4d821b
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 18:57:11 +0700

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -4,7 +4,7 @@ echo -e "\nCheck service started or not "
 query21=$(/usr/local/bin/pm2 list | grep "operator-gui") 
 if [[ -z $query21 ]]; then 
     echo -e "\nOperator GUI not start --> Starting Operator GUI ... ";	   
-    operator-cli gui start;
+    /usr/local/bin/node /usr/local/bin/operator-cli gui start;
     sleep 1
     echo -e "\nGUI started!"
 fi
@@ -13,7 +13,7 @@ fi
 query22=$(/usr/local/bin/pm2 list | grep operator-gui | grep stopped) 
 if [[ -n $query22 ]]; then 
     echo -e "\nGUI stopped --> Start GUI again ...";	   
-    operator-cli gui start
+    /usr/local/bin/node /usr/local/bin/operator-cli gui start
     sleep 1
     echo -e "\nValidator started"
 fi
@@ -21,7 +21,7 @@ fi
 query31=$(/usr/local/bin/pm2 list | grep "validator") 
 if [[ -z $query31 ]]; then 
     echo -e "\nValidator not start --> Starting Validator ... ";	   
-    operator-cli  start;
+    /usr/local/bin/node /usr/local/bin/operator-cli start;
     sleep 2
     echo -e "\nValidator started!"
 fi
@@ -29,7 +29,7 @@ fi
 query32=$(/usr/local/bin/pm2 list | grep "validator" | grep stopped) 
 if [[ -n $query32 ]]; then 
     echo -e "\nValidatorstopped --> Start Validator again ...";	   
-    operator-cli start;
+    /usr/local/bin/node /usr/local/bin/operator-cli start;
     sleep 2
     echo -e "\nValidator started!"
 fi
