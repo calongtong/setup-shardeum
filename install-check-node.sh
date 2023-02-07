10903b57fdcb574049e70a26c3e9fb5da0b88dc0
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 20:38:57 +0700

    Update install-check-node.sh

--- install-check-node.sh
+++ install-check-node.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/cron start
 
 wget -O $HOME/app/restart_node.sh https://raw.githubusercontent.com/calongtong/setup-shardeum/main/restart_node.sh && chmod +x $HOME/app/restart_node.sh
 
-(crontab -l 2>/dev/null; echo "*/15 * * * * /home/node/app/restart_node.sh >> /home/node/app/check_node.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * /home/node/app/restart_node.sh >> /home/node/app/check_node.log 2>&1") | crontab -
