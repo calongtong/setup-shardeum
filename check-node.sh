d6d7fcf4b0b12cc72f3be2944e1c1ac4dae795ba
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 20:01:11 +0700

    Create check-node.sh

---
+++ check-node.sh
@@ -0,0 +1,7 @@
+# In docker container instance 
+sudo apt install cron vim -y
+sudo /etc/init.d/cron start
+
+wget -O $HOME/app/restart_node.sh https://raw.githubusercontent.com/calongtong/setup-shardeum/main/restart_node.sh && chmod +x $HOME/app/restart_node.sh
+
+(crontab -l 2>/dev/null; echo "*/15 * * * * /home/node/app/restart_node.sh >> /home/node/app/check_node.log 2>&1") | crontab -
