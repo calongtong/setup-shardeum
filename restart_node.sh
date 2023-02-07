7c5700d5af50e571674c0fb5c7ba5b890475c34e
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-07 18:05:35 +0700

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -7,6 +7,14 @@ if [[ -n $query ]]; then
     echo -n "Validator started"
 fi
 
+query21=$(/usr/local/bin/pm2 list | grep "operator-gui") 
+if [[ -n $query21 ]]; then 
+    echo -n "#Operator GUI not start --> Starting Operator GUI ";	   
+    operator-cli gui start;
+    echo -n "GUI started!"
+fi
+sleep 3
+
 query2=$(/usr/local/bin/pm2 list | grep "operator-gui" | grep stopped) 
 if [[ -n $query2 ]]; then 
     echo -n "#Operator GUI stopped --> Start Operator GUI again ...";	   
